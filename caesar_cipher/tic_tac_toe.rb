60ae163d39c07641d9d145ef124ce98856d94fd1
Taylor Hill <64454243+taylorhill0413@users.noreply.github.com>
2025-07-06 06:13:58 +0700

    done exercise tik tac toe

--- caesar_cipher/tic_tac_toe.rb
+++ caesar_cipher/tic_tac_toe.rb
@@ -34,70 +34,74 @@ class TikTacTie
   def play_game
     puts 'Input match coordinate format: row,column'
     loop do
-      print 'User A:'
-      data = gets.chomp
-      print data
-      input_user('A', data.split(',')[0].to_i, data.split(',')[1].to_i)
-      print 'User B:'
-      data = gets.chomp
-      input_user('B', data.split(',')[0].to_i, data.split(',')[1].to_i)
+      input_user('A')
       result = check_condition(arr)
-      break unless result.nil?
+      unless result.nil?
+        puts result
+        break
+      end
+      input_user('B')
+      result = check_condition(arr)
+      # puts "#{result} - #{arr}"
+      unless result.nil?
+        puts result
+        break
+      end
     end
   end
 
-  def input_user(user, row, column)
-    _sym = if user == 'A'
-             'X'
-           else
-             'O'
-           end
-    if row.to_i.positive? == false || column.to_i.positive? == false || row > rows || column > columns
-      print 'Error input rows or columns'
+  def input_user(user)
+    5.times do
+      print "User #{user}:"
+      data = gets.chomp
+      row = data.split(',')[0].to_i
+      column = data.split(',')[1].to_i
+      if arr[row - 1][column - 1] != '☐'
+        puts "Error input: This position is already taken"
+        next
+      end
+      _sym = if user == 'A'
+               'X'
+             else
+               'O'
+             end
+      if row.to_i.positive? == false || column.to_i.positive? == false || row > rows || column > columns
+        puts 'Error input rows or columns'
+        next
+      end
+      arr[row - 1][column - 1] = _sym
+      clear_output
+      display_board(arr)
       return
     end
-    arr[row - 1][column - 1] = _sym
-    clear_output
-    display_board(arr)
+    puts "Error input"
+    
   end
 
   def result(arr, target)
-    arr.any? do |row|
-      row.each_cons(target.length).any? { |subarray| subarray == target }
+    for i in 0..arr.length - 2
+      for j in 0..arr[i].length - 2
+        if arr[i][j] == target && arr[i][j + 1] == target && arr[i][j + 2] == target
+          return true
+        elsif arr[i][j] == target && arr[i + 1][j] == target && arr[i + 2][j] == target
+          return true
+        elsif arr[i][j] == target && arr[i + 1][j + 1] == target && arr[i + 2][j + 2] == target
+          return true
+        else
+          return false
+        end
+      end
     end
   end
 
   def check_condition(arr)
-    if result(arr, '%w[X X X]')
+    if result(arr, 'X')
       return 'A win'
-    elsif result(arr, '%w[O O O]')
-      return 'B win'
-    end
-
-    arr_column = Array.new(rows)
-    arr.each_with_index do |item, index|
-      arr_temp = []
-      item.each_with_index do |_item, _index|
-        arr_temp.push(arr[_index][index])
-      end
-      arr_column[index] = arr_temp
-    end
-
-    if result(arr_column, '%w[X X X]')
-      return 'A win'
-    elsif result(arr_column, '%w[O O O]')
+    elsif result(arr, 'O')
       return 'B win'
     end
 
     nil
-    # arr_diagonal = Array.new(rows)
-    # arr.each_with_index do |item, index|
-    #   arr_temp = []
-    #   item.each_with_index do |_item, _index|
-    #     arr_temp.push(arr[_index + index][_index])
-    #   end
-    #   arr_diagonal[index] = arr_temp
-    # end
   end
 end
 
