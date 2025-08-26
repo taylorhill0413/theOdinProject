3b669badf97b854a3109f719142a04dfe27ccb6f
Taylor Hill <taylorhill0413@gmail.com>
2025-08-26 13:50:01 +0700

    game chess

---
+++ project_chess/lib/board.rb
@@ -0,0 +1,14 @@
+# class Board
+#   attr_reader :grid
+
+#   ROWS = 7
+#   def initialize
+#     @grid = Array.new(ROWS) { Array.new(ROWS, nil) }
+#   end
+
+#   def start_board(player)
+#     player_A = ["U+2656", "U+2658"]
+#   end
+# end
+
+print 'U+2656'
